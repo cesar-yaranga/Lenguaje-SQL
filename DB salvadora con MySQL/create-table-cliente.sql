aa73cd58831880f0ac866ec08d6360e619c7776b
cesar-yaranga <juliocesar3653@gmail.com>
2021-05-11 18:39:18 -0500

    Creacion de la base de datos salvadora

---
+++ DB salvadora con MySQL/create-table-cliente.sql
@@ -0,0 +1,9 @@
+USE salvadora;
+CREATE TABLE cliente(
+	ci INT (10) NOT NULL,
+    nombre VARCHAR(20),
+    apellido VARCHAR (20),
+    direccion TEXT (50),
+    telefono INT (8),
+    PRIMARY KEY (ci)
+);
